f1479a1e809ce7846ac9dec637ff4a32bc551686
Giten Mitra <gitenmitra@gmail.com>
2021-05-27 18:23:05 +0530

    Add files via upload

---
+++ DNS-PING.py
@@ -0,0 +1,27 @@
+import json
+import urllib.request
+import logging
+import boto3
+from urllib.error import URLError, HTTPError
+client = boto3.client('sns')
+import subprocess
+
+
+logger = logging.getLogger()
+def test():
+    try:
+     statuscode = urllib.request.urlopen("http://www.amazon.in").getcode()
+    except URLError as error:
+        logger.error("Server connection failed: %s", error.reason)
+        prog = subprocess.run(["ping", "11.11.11.11"], capture_output=True)
+        print("Output is \n",prog.stdout.decode())
+        response = client.publish(
+            TopicArn='arn:aws:sns:us-east-1:1111111111:SecurityGroup-Test1234',
+            Message='prog',
+            Subject='Server connection failed'
+            )
+    else:
+        print("Everythng working fine")
+    return
+
+test()
